17abea1d9f426e843539e6fead0c0f88100b079e
Subham Das <52673448+subhamdas461@users.noreply.github.com>
2021-10-11 13:01:53 +0530

    Revert "Added match results to webpage and removed the prompt"

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 var cube = document.getElementsByClassName("box");
-let msg = document.getElementById("msg");
-let text = document.querySelector("p");
 var count = 0;
+let turn = 1;
 
-function func(ths)
-{
+function func(ths) {
+    let playerTurn = document.getElementsByClassName("player_turn");
+    if (turn === 1)
+        playerTurn[0].innerHTML = "Player 2 Turn";
+    else if (turn === 2)
+        playerTurn[0].innerHTML = "Player 1 Turn"; ths.classList.add("click-disable");
     ths.classList.add("click-disable");
     count++;
-    if (count % 2 == 0)
-    {
-        ths.innerHTML = "X";
+    if (count % 2 == 0) {
+        ths.innerHTML = "x";
         ths.accessKey = 1;
-    } else
-    {
+        turn = 1;
+    } else {
         ths.innerHTML = "O";
         ths.accessKey = 0;
+        turn = 2;
     }
     const a = parseInt(cube[0].accessKey);
     const b = parseInt(cube[1].accessKey);
@@ -34,15 +37,9 @@ function func(ths)
         c + f + i == 3 ||
         a + e + i == 3 ||
         c + e + g == 3
-    )
-    {
-        text.innerHTML = `✨🎇🎆 X won the match! 🎉🎊`;
-        msg.style.visibility = "visible";
-        setTimeout(() =>
-        {
-            restart();
-        }, 4000);
-
+    ) {
+        alert("X wins!! ");
+        restart();
     } else if (
         a + b + c == 0 ||
         d + e + f == 0 ||
@@ -52,27 +49,15 @@ function func(ths)
         c + f + i == 0 ||
         a + e + i == 0 ||
         c + e + g == 0
-    )
-    {
-        text.innerHTML = `✨🎇🎆 O won the match! 🎉🎊`;
-        msg.style.visibility = "visible";
-        setTimeout(() =>
-        {
-            restart();
-        }, 4000);
-    } else if (a + b + c + d + e + f + g + h + i)
-    {
-        text.innerHTML = `Match Draw !!!`;
-        msg.style.visibility = "visible";
-        setTimeout(() =>
-        {
-            restart();
-        }, 4000);
-
+    ) {
+        alert("O wins!!  ");
+        restart();
+    } else if (a + b + c + d + e + f + g + h + i) {
+        alert("Its a Draw !!!");
+        restart();
     }
 }
 
-function restart()
-{
+function restart() {
     window.location.reload();
-}
+} 
