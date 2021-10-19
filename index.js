bdd27af5ef68e7c6b937d5bcd54043dc5a95cbef
Sujeet Kumar <sujeet7982422191@gmail.com>
2021-10-19 16:48:23 +0530

    Optimised the code

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var cube = document.getElementsByClassName("box");
 var count = 0;
-let turn = 1;
+let turn = green = "#5ef141";
 
 function func(ths) {
     let playerTurn = document.getElementsByClassName("player_turn");
@@ -37,43 +37,40 @@ function func(ths) {
         a + e + i == 3 ||
         c + e + g == 3
     ) {
-<<<<<<< HEAD
         if (a + b + c == 3) {
-            document.querySelector(".one").style.backgroundColor = "#5ef141";
-            document.querySelector(".two").style.backgroundColor = "#5ef141";
-            document.querySelector(".three").style.backgroundColor = "#5ef141";
+            cube[0].style.backgroundColor = green;
+            cube[1].style.backgroundColor = green;
+            cube[2].style.backgroundColor = green;
         }else if(d + e + f == 3){
-            document.querySelector(".four").style.backgroundColor = "#5ef141";
-            document.querySelector(".five").style.backgroundColor = "#5ef141";
-            document.querySelector(".six").style.backgroundColor = "#5ef141";
+            cube[3].style.backgroundColor = green;
+            cube[4].style.backgroundColor = green;
+            cube[5].style.backgroundColor = green;
         }else if(g + h + i == 3){
-            document.querySelector(".seven").style.backgroundColor = "#5ef141";
-            document.querySelector(".eight").style.backgroundColor = "#5ef141";
-            document.querySelector(".nine").style.backgroundColor = "#5ef141";
+            cube[6].style.backgroundColor = green;
+            cube[7].style.backgroundColor = green;
+            cube[8].style.backgroundColor = green;
         }else if(a + d + g == 3){
-            document.querySelector(".one").style.backgroundColor = "#5ef141";
-            document.querySelector(".four").style.backgroundColor = "#5ef141";
-            document.querySelector(".seven").style.backgroundColor = "#5ef141";
+            cube[0].style.backgroundColor = green;
+            cube[3].style.backgroundColor = green;
+            cube[6].style.backgroundColor = green;
         }else if(b + e + h == 3){
-            document.querySelector(".two").style.backgroundColor = "#5ef141";
-            document.querySelector(".five").style.backgroundColor = "#5ef141";
-            document.querySelector(".eight").style.backgroundColor = "#5ef141";
+            cube[1].style.backgroundColor = green;
+            cube[4].style.backgroundColor = green;
+           cube[7].style.backgroundColor = green;
         }else if(c + f + i == 3){
-            document.querySelector(".three").style.backgroundColor = "#5ef141";
-            document.querySelector(".six").style.backgroundColor = "#5ef141";
-            document.querySelector(".nine").style.backgroundColor = "#5ef141";
+            cube[2].style.backgroundColor = green;
+            cube[5].style.backgroundColor = green;
+            cube[8].style.backgroundColor = green;
         }else if(a + e + i == 3){
-            document.querySelector(".one").style.backgroundColor = "#5ef141";
-            document.querySelector(".five").style.backgroundColor = "#5ef141";
-            document.querySelector(".nine").style.backgroundColor = "#5ef141";
+            cube[0].style.backgroundColor = green;
+            cube[4].style.backgroundColor = green;
+            cube[8].style.backgroundColor = green;
         }else if(c + e + g == 3){
-            document.querySelector(".three").style.backgroundColor = "#5ef141";
-            document.querySelector(".five").style.backgroundColor = "#5ef141";
-            document.querySelector(".seven").style.backgroundColor = "#5ef141";
+            cube[2].style.backgroundColor = green;
+            cube[4].style.backgroundColor = green;
+            cube[6].style.backgroundColor = green;
         }
 
-=======
->>>>>>> 1363942c1b3e06678fe32aeb9099108c60c092e5
         //alert("X wins!! ");
         document.getElementById("result").innerText = "Player 2 (X) wins!!";
         on();
@@ -90,43 +87,40 @@ function func(ths) {
         //alert("O wins!!  ");
         document.getElementById("result").innerText = "Player 1 (O) wins!!";
         on();
-<<<<<<< HEAD
 
-        if (a + b + c == 3) {
-            document.querySelector(".one").style.backgroundColor = "#5ef141";
-            document.querySelector(".two").style.backgroundColor = "#5ef141";
-            document.querySelector(".three").style.backgroundColor = "#5ef141";
+        if (a + b + c == 0) {
+            cube[0].style.backgroundColor = green;
+            cube[1].style.backgroundColor = green;
+            cube[2].style.backgroundColor = green;
         }else if(d + e + f == 0){
-            document.querySelector(".four").style.backgroundColor = "#5ef141";
-            document.querySelector(".five").style.backgroundColor = "#5ef141";
-            document.querySelector(".six").style.backgroundColor = "#5ef141";
+            cube[3].style.backgroundColor = green;
+            cube[4].style.backgroundColor = green;
+            cube[5].style.backgroundColor = green;
         }else if(g + h + i == 0){
-            document.querySelector(".seven").style.backgroundColor = "#5ef141";
-            document.querySelector(".eight").style.backgroundColor = "#5ef141";
-            document.querySelector(".nine").style.backgroundColor = "#5ef141";
+            cube[6].style.backgroundColor = green;
+            cube[7].style.backgroundColor = green;
+            cube[8].style.backgroundColor = green;
         }else if(a + d + g == 0){
-            document.querySelector(".one").style.backgroundColor = "#5ef141";
-            document.querySelector(".four").style.backgroundColor = "#5ef141";
-            document.querySelector(".seven").style.backgroundColor = "#5ef141";
+            cube[0].style.backgroundColor = green;
+            cube[3].style.backgroundColor = green;
+            cube[6].style.backgroundColor = green;
         }else if(b + e + h == 0){
-            document.querySelector(".two").style.backgroundColor = "#5ef141";
-            document.querySelector(".five").style.backgroundColor = "#5ef141";
-            document.querySelector(".eight").style.backgroundColor = "#5ef141";
+            cube[1].style.backgroundColor = green;
+            cube[4].style.backgroundColor = green;
+           cube[7].style.backgroundColor = green;
         }else if(c + f + i == 0){
-            document.querySelector(".three").style.backgroundColor = "#5ef141";
-            document.querySelector(".six").style.backgroundColor = "#5ef141";
-            document.querySelector(".nine").style.backgroundColor = "#5ef141";
+            cube[2].style.backgroundColor = green;
+            cube[5].style.backgroundColor = green;
+            cube[8].style.backgroundColor = green;
         }else if(a + e + i == 0){
-            document.querySelector(".one").style.backgroundColor = "#5ef141";
-            document.querySelector(".five").style.backgroundColor = "#5ef141";
-            document.querySelector(".nine").style.backgroundColor = "#5ef141";
+            cube[0].style.backgroundColor = green;
+            cube[4].style.backgroundColor = green;
+            cube[8].style.backgroundColor = green;
         }else if(c + e + g == 0){
-            document.querySelector(".three").style.backgroundColor = "#5ef141";
-            document.querySelector(".five").style.backgroundColor = "#5ef141";
-            document.querySelector(".seven").style.backgroundColor = "#5ef141";
+            cube[2].style.backgroundColor = green;
+            cube[4].style.backgroundColor = green;
+            cube[6].style.backgroundColor = green;
         }
-=======
->>>>>>> 1363942c1b3e06678fe32aeb9099108c60c092e5
     } else if (a + b + c + d + e + f + g + h + i) {
         //alert("Its a Draw !!!");
         document.getElementById("result").innerText = "It's a Draw!!";
